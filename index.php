d17a57b645539e41a3818f25c1964eb92ee13802
muhamadjihad <92348649+muhamadjihad@users.noreply.github.com>
2021-11-09 20:58:57 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK website</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMK website</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+    <?php if(isset($_GET['status'])): ?>
+    <p>
+        <?php
+            if($_GET['status'] == 'sukses'){
+                echo "Pendaftaran siswa baru berhasil!";
+            } else {
+                echo "Pendaftaran gagal!";
+            }
+        ?>
+    </p>
+<?php endif; ?>
+    </body>
+</html>
